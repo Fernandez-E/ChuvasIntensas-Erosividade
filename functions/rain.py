a44b4a2f8fc6aa84fc57c323f7af83ae5ddb79a1
Eric G. Fernandez A. S. <efernandezalbuq@gmail.com>
2025-08-16 02:57:38 -0300

    rainfall events

--- functions/rain.py
+++ functions/rain.py
@@ -1,20 +1,49 @@
-from datetime import timedelta
-from time import sleep
+from datetime import timedelta, datetime
+
+events = []
+events_indexes = []
+
 
 def rainfall_events(rain_data: list):
-    max = len(rain_data[1])
-    print(max)
-    for x in range(1, max):
-        start = 0
-        end = x
-        sleep(1)
-        print(f'------------------------------------')
-        while end < max:
-            if rain_data[0][start] - rain_data[0][end] > timedelta(hours=6):
-                print(rain_data[0][start] - rain_data[0][end])
-                break
+    # 6h
+    max_index = len(rain_data[1])
+    start = 0
+
+    while start + 72 <= max_index:
+        block = rain_data[1][start:start + 72]
+
+        if block[0] != 0:  # verifica se primeiro valor é diferente de zero
+            total = sum(block)
+            if total >= 10:
+                print(f"Evento de até 6 horas detectado de {total}mm: {block}")
+                events.append([rain_data[0][start], rain_data[0][start + 72 - 1], total])
+                events_indexes.extend(range(start, start + 72))
+                start += 72
             else:
-                rain = rain_data[1][start:end]
-                print(f'Inicio: {rain_data[0][start]} - Fim: {rain_data[0][end]} - Soma: {sum(rain)}')
-            start+=1
-            end+=1
+                start += 1
+        else:
+            start += 1
+
+    # 15min
+    start = 0
+
+    while start + 3 <= max_index:
+        if any(i in events_indexes for i in range(start, start + 3)):
+            start += 1
+            continue
+
+        block = rain_data[1][start:start + 3]
+
+        if block[0] != 0:  # começa com valor diferente de zero
+            total = sum(block)
+            if total > 6:
+                print(f"Evento de até 15 minutos detectado de {total:.3f}mm: {block}")
+                events.append([rain_data[0][start], rain_data[0][start + 3 - 1], total, "Evento 15min"])
+                events_indexes.extend(range(start, start + 3))
+                start += 3
+            else:
+                start += 1
+        else:
+            start += 1
+
+    return events
--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 from functions import files, rain
 
-rain_data = files.cr1000_data("CR1000_Table5min_2017_02_23_14_52_58.dat")
-# RAIN_DATA[0] = Data e hora
-# RAIN_DATA[1] = Precipitação
-
-
-rain.rainfall_events(rain_data)
+print(rain.rainfall_events(files.cr1000_data("CR1000_Table5min_2017_02_23_14_52_58.dat")))
